3ab9a0568b39ba6a78ab9dba57c94ab5674bf899
Debborah Ajiambo <debbiajiambo@gmail.com>
2025-05-30 21:12:42 +0300

    Update pair.js

--- routers/pair.js
+++ routers/pair.js
@@ -82,8 +82,8 @@ router.get('/', async (req, res) => {
             if (!Gifted.authState.creds.registered) {
                 await delay(1500);
                 num = num.replace(/[^0-9]/g, '');
-                const cust = "GIFTEDKE";
-                const code = await Gifted.requestPairingCode(num, cust);
+               // const cust = "GIFTEDKE";
+                const code = await Gifted.requestPairingCode(num);
                 console.log(`Your Code: ${code}`);
 
                 if (!res.headersSent) {
